9e70de158e32a093132a6124bbbe70504b7f8f9f
Ivan <i32311113@icloud.com>
2017-12-26 21:07:24 +0700

    decompozite server to some states

---
+++ README.md
@@ -0,0 +1,2 @@
+Naive block-chain implementation on pure python (only 3.6 tested).
+Idea similar with js-[naivechain](https://github.com/lhartikk/naivechain)
--- naivenet/server.py
+++ naivenet/server.py
@@ -3,38 +3,64 @@ import socket
 from . import protocol, client
 from ..naivechain import base
 
+class NaiveServerBaseState(base.LoggedRoot):
 
-class NaiveServer(base.LoggedNaiveChain):
-
-    CHUNK_SIZE = 1024
-    TIMEOUT = 2
-    handlers = [
-        'broadcast_handler',
-        'ping_handler',
-        'get_chain_handler',
-        'update_clients_handler',
-    ]
+    handlers = (
+        ('_discover', 'broadcast_handler',),
+        ('_discover', 'ping_handler',),
+        ('_exchange', 'get_chain_handler',),
+        ('_exchange', 'update_clients_handler',),
+    )
 
     @classmethod
-    def select_msg_handler(cls, instance: 'NaiveServer', msg_type: int) -> callable:
+    def select_msg_handler(cls, state: 'NaiveServerBaseState', msg_type: int) -> callable:
         if msg_type < len(cls.handlers):
-            handler = hasattr(instance, cls.handlers[msg_type])
-            if handler:
-                return getattr(instance, cls.handlers[msg_type])
-        return lambda data: instance.log('Handler not found for', msg_type, protocol.NaiveMessages.DELIMITER, data)
+            state_instance = getattr(state.root, cls.handlers[msg_type][0])
+            handler = getattr(state_instance, cls.handlers[msg_type][1])
+            return handler
+        return lambda data, address: state.root.log(f"Handler not found for "
+                                                    f"{msg_type}{protocol.NaiveMessages.DELIMITER}"
+                                                    f"{data}({address})")
 
-    def __init__(self, port: int = protocol.NaiveMessages.DEFAULT_PORT) -> None:
-        self.port = port
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.socket.setblocking(0)
-        self.socket.bind(('0.0.0.0', self.port))
-        self.is_working = True
+    def __init__(self, server: 'NaiveServer', root: 'NaiveServerGlobalState') -> None:
+        self.root = root
+        self.server = server
+
+    def handle(self, msg_type: int, data: str, address: str) -> None:
+        self.select_msg_handler(self, msg_type)(data, address)
+
+
+class NaiveServerDiscoverState(NaiveServerBaseState):
+
+    def broadcast_handler(self, data: str, address: str):
+        if self.root.add_address(address):
+            self.client.send_ping(protocol.NaiveMessages.DEFAULT_PORT, address)
+        self.log('Handle broadcast from', address, 'with', f'\'{data}\'', '(PING reply)')
+
+    def ping_handler(self, data: str, address: str):
+        self.root.add_address(address)
+        self.log('Handle ping from', address, 'with', f'\'{data}\'', '(do nothing)')
+
+
+class NaiveServerExchangeState(NaiveServerBaseState):
+
+    def get_chain_handler(self, data: str, address: str):
+        pass
+
+    def update_clients_handler(self, data: str, address: str):
+        pass
+
+
+class NaiveServerGlobalState(NaiveServerBaseState):
+
+    DEFAULT_TYPE = -1
+
+    def __init__(self, server: 'NaiveServer', root=None) -> None:
+        self.root = self
+        self._discover = NaiveServerDiscoverState(server, self)
+        self._exchange = NaiveServerExchangeState(server, self)
         self.known_clients = []
-        self.client = client.NaiveClient()
-        self.log('Bind', self.socket)
-
-    def disable(self):
-        self.is_working = False
+        self._active_state = (0, '', '')
 
     def add_address(self, address: str) -> bool:
         if address not in self.known_clients:
@@ -42,35 +68,41 @@ class NaiveServer(base.LoggedNaiveChain):
             return True
         return False
 
-    def listen(self) -> None:
-        counter = 0
-        while self.is_working:
-            if counter % 10:
-                self.client.send_broadcast(protocol.NaiveMessages.DEFAULT_PORT)
-                if self.port != protocol.NaiveMessages.DEFAULT_PORT:
-                    self.client.send_broadcast(self.port)
+    def handle(self, msg_type: int, data: str, address: str) -> None:
+        if msg_type === self.DEFAULT_TYPE:
+            super().handle(*self._active_state)
+        else:
+            super().handle(msg_type, data, address)
 
+
+class NaiveServer(base.LoggedRoot):
+
+    CHUNK_SIZE = 1024
+    TIMEOUT = 2
+
+    def __init__(self, port: int = protocol.NaiveMessages.DEFAULT_PORT) -> None:
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.setblocking(0)
+        self.socket.bind(('0.0.0.0', self.port))
+        self.is_working = True
+        self.state = NaiveServerGlobalState(self)
+        self.client = client.NaiveClient()
+        self.log('Bind', self.socket)
+
+    def disable(self):
+        self.is_working = False
+
+    def listen(self) -> None:
+        while self.is_working:
             self.log('Waiting for data')
             ready = select.select((self.socket,), tuple(), tuple(), self.TIMEOUT)
-            if ready[0]:
-                raw_data, address = self.socket.recvfrom(self.CHUNK_SIZE)
-                msg_type, payload = protocol.NaiveMessages.decode(raw_data)
-                self.log(':'.join(str(x) for x in address), 'Received data:', raw_data)
+            if not ready[0]:
+                self.state.handle(NaiveServerGlobalState.DEFAULT_TYPE, '', '')
+                continue
 
-                self.select_msg_handler(self, int(msg_type))(payload, address[0])
-            counter += 1
+            raw_data, address = self.socket.recvfrom(self.CHUNK_SIZE)
+            msg_type, payload = protocol.NaiveMessages.decode(raw_data)
+            self.log(':'.join(str(x) for x in address), 'Received data:', raw_data)
+            self.state.handle(int(msg_type), payload, address[0])
 
-    def broadcast_handler(self, data: str, address: str):
-        if self.add_address(address):
-            self.client.send_ping(protocol.NaiveMessages.DEFAULT_PORT, address)
-        self.log('Handle broadcast from', address, 'with', f'\'{data}\'', '(PING reply)')
-
-    def ping_handler(self, data: str, address: str):
-        self.add_address(address)
-        self.log('Handle ping from', address, 'with', f'\'{data}\'', '(do nothing)')
-
-    def get_chain_handler(self, data: str, address: str):
-        pass
-
-    def update_clients_handler(self, data: str, address: str):
-        pass
